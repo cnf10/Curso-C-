cd7bc61923558de00ae5d8309ebd71a45879ce48
cnf10 <ofelix39@gmail.com>
2018-02-10 09:50:43 -0200

    Adicionado a Aula04 sobre Estrutura Condicional (While)!

---
+++ Aula04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade, soma, count;
+            double media;
+
+            soma = 0;
+            count = 0;
+
+            idade = int.Parse(Console.ReadLine());
+
+        while (idade >= 0) {
+            soma = soma + idade;
+            count = count + 1;
+            //count++ Tambem pode ser utilizado;
+            idade = int.Parse(Console.ReadLine());
+        }
+
+        if(count == 0){
+            Console.WriteLine("IMPOSSIVEL CALCULAR");
+            
+        }else{
+
+        media = (double)soma / count;
+        Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        Console.ReadLine();
+
+
+
+
+            
+        }
+    }
+}
