8e454e55dcedb76ab5d93ed59dc0e81034c8153a
cnf10 <ofelix39@gmail.com>
2018-02-10 10:22:18 -0200

    Adicionado a Aula05 sobre Estrutura (For)!

---
+++ Aula05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int N;
+           double x, y, resultado;
+
+           N = int.Parse(Console.ReadLine());
+
+           for (int i=0; i<N; i++ ){
+               string [] vet = Console.ReadLine().Split(' ');
+               x= double.Parse(vet[0], CultureInfo.InvariantCulture);
+               y= double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+               if(y == 0.0){
+                   Console.WriteLine("NÃO EXISTE!");
+
+               }else{
+                   resultado = x / y;
+                   Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture));
+               }
+           }
+           Console.ReadLine();
+        }
+    }
+}
